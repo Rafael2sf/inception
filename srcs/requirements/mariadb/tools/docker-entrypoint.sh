9a742c3c22cbd293fdc9fd70f3515200d3b19fb3
Rafael2sf <rafaelmdsbni@gmail.com>
2022-10-17 17:52:10 +0100

    improved entrypoint scripts, added environt variables usage

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,43 +1,69 @@
 #!/bin/bash
 
+function check_envars()
+{
+	for ARG in "$@"; do
+		if [ ! "${ARG#*=}" ]; then
+			echo "[ERROR] $ARG: invalid environment variable" >&2
+			exit 1
+		fi
+	done
+}
+
 function mysql_cmd()
 {
-	/usr/bin/mariadb -u root -proot -e "$1"
+	/usr/bin/mariadb -u root -p${MYSQL_ROOT_PASSWORD} -e "$1"
 	if [ $? -ne 0 ]; then
 		echo "failed running mysql command: $1" >&2
 		exit 1
 	fi
 }
 
-/usr/bin/mariadb-install-db \
-	--defaults-file=/etc/my.cnf.d/mariadb-server.cnf \
-	--auth-root-authentication-method=socket \
-	--auth-root-socket-user=root \
-	--datadir=/var/lib/mysql \
-	--skip-test-db
+function run_safe()
+{
+	$1
+	if [ $? -ne 0 ]; then
+		echo "failed running command: $1" >&2
+		exit 1
+	fi
+}
 
-# init mysql
-/usr/bin/mariadbd-safe
-/usr/bin/mariadb-admin shutdown
-/usr/bin/mariadbd-safe
 
-# set root pass without exiting if fails
-/usr/bin/mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'"
+rc-status -a > /dev/null
 
-#if [ $? -eq 0 ]; then
-#	mysql_cmd "DROP USER IF EXISTS ''@'localhost'"
-#	mysql_cmd "DROP USER IF EXISTS ''@'$(hostname)'"
-#	mysql_cmd "DROP DATABASE IF EXISTS test"
-#fi
-#
-## Create provided database and user if dont exist
-#mysql_cmd "CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'user'"
-#mysql_cmd "CREATE DATABASE IF NOT EXISTS test_db"
-#mysql_cmd "GRANT ALL PRIVILEGES ON test_db.* TO 'user'@'%' IDENTIFIED BY 'user'"
-#mysql_cmd "FLUSH PRIVILEGES"
-#/usr/bin/mariadb -u root -e < /data/test.sql
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	check_envars \
+		"MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}" \
+		"MYSQL_DATABASE=${MYSQL_DATABASE}" \
+		"MYSQL_USER=${MYSQL_USER}" \
+		"MYSQL_USER_PASSWORD=${MYSQL_USER_PASSWORD}"
+	# init
+	run_safe "/usr/bin/mariadb-install-db \
+		--defaults-file=/etc/my.cnf.d/mariadb-server.cnf"
+	run_safe "rc-service mariadb start"
 
-# shutdown
-/usr/bin/mariadb-admin shutdown
+	# config db
+	/usr/bin/mariadb -u root -e "ALTER USER 'root'@'localhost' \
+		IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'"
+	if [ $? -ne 0 ]; then
+		echo "[ERROR]: Failed to set root password" >&2
+		exit 2
+	fi
+	mysql_cmd "DROP USER IF EXISTS ''@'localhost'"
+	mysql_cmd "DROP USER IF EXISTS ''@'$(hostname)'"
+	mysql_cmd "DROP DATABASE IF EXISTS test"
+	mysql_cmd "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}"
+	mysql_cmd "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' \
+		IDENTIFIED BY '${MYSQL_USER_PASSWORD}'"
+	mysql_cmd "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' \
+		IDENTIFIED BY '${MYSQL_USER_PASSWORD}'"
+	mysql_cmd "FLUSH PRIVILEGES"
+
+	# shutdown
+	run_safe "/usr/bin/mysqladmin shutdown -p${MYSQL_ROOT_PASSWORD}"
+else
+	echo "[WARNING]: Detected '/var/lib/mysql/mysql/': \
+database already exists, ignoring environment variables" >&2
+fi
 
 exec /usr/bin/mariadbd-safe
--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,21 +1,78 @@
 #!/bin/bash
 
-wp core download
-if [ $? -eq 0 ]; then
-	wp config create --dbname=test_db --dbuser=user --dbpass=user --dbhost=mdb
+function check_envars()
+{
+	for ARG in "$@"; do
+		if [ ! "${ARG#*=}" ]; then
+			echo "[ERROR] $ARG: invalid environment variable" >&2
+			exit 1
+		fi
+	done
+}
+
+function wait_port()
+{
+	local TIMEOUT_COUNTER=0
+
+	while ! nc -z $1 $2; do
+		((TIMEOUT_COUNTER=TIMEOUT_COUNTER+2))
+		if [ $TIMEOUT_COUNTER -eq $3 ]; then
+			echo "[ERROR]: connection to database: timeout" >&2
+			exit 1
+		fi
+		echo "[-]: connection to database: atempt failed" >&2
+		sleep 2
+	done
+	echo "[OK]: connection to database: success" >&2
+}
+
+function error_status()
+{
+	if [ $? -ne 0 ]; then
+		echo "[ERROR]: $1"
+		exit 1
+	fi
+}
+
+wait_port "mdb" "3306" 30
+
+if [ ! -f "wp-config.php" ]; then
+	check_envars \
+		"MYSQL_DATABASE=${MYSQL_DATABASE}" \
+		"MYSQL_USER=${MYSQL_USER}" \
+		"MYSQL_USER_PASSWORD=${MYSQL_USER_PASSWORD}" \
+		"WP_ADMIN=${WP_ADMIN}" \
+		"WP_ADMIN_PASSWORD=${WP_ADMIN_PASSWORD}" \
+		"WP_ADMIN_EMAIL=${WP_ADMIN_EMAIL}" \
+		"WP_USER=${WP_USER}" \
+		"WP_USER_PASSWORD=${WP_USER_PASSWORD}" \
+		"WP_USER_EMAIL=${WP_USER_EMAIL}"
+
+	# init config
+	wp config create \
+		--dbname=${MYSQL_DATABASE} \
+		--dbuser=${MYSQL_USER} \
+		--dbpass=${MYSQL_USER_PASSWORD} \
+		--dbhost=mdb
+	error_status "failed to create config"
+
+	# core install with administrator
 	chmod 644 wp-config.php
 	wp core install \
 		--url=rafernan.42.fr \
-		--title="test_web" \
-		--admin_name=admin \
-		--admin_password=admin \
-		--admin_email=rafaelmdsbni@gmail.com
-else
-	wp core config --dbuser=user --dbpass=user
-fi
+		--title=inception \
+		--admin_name=${WP_ADMIN} \
+		--admin_password=${WP_ADMIN_PASSWORD} \
+		--admin_email=${WP_ADMIN_EMAIL}
+	error_status "failed to install wp"
 
-#	echo "must provide all of the following environment \
-#variables: MYSQL_DATABASE MYSQL_USER MYSQL_USER_PASSWORD" >&2
-#	exit 1
+	# Create user
+	wp user create ${WP_USER} ${WP_USER_EMAIL} \
+		--user_pass=${WP_USER_PASSWORD} --role=author
+	error_status "failed to create user on wp database"
+else
+	echo "[WARNING]: Detected 'wp-config.php': \
+wordpress already exists, ignoring environment variables" >&2
+fi
 
 exec /usr/sbin/php-fpm8 -F -R
